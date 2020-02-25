0760f4e7367443f2f31198b66c3a5b2e1f50af9f
md-msig <mdmsig20@gmail.com>
2020-02-25 10:45:29 +0200

    create group & resource crud controller

---
+++ app/Http/Controllers/ResourceCRUDControler.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Resource;
+
+class ResourceCRUDControler extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::orderBy('id','DESC')->paginate(5);
+        return view('ResourceCRUD.index',compact('items'))
+            ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('ResourceCRUD.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'group_id' => 'required|exists:group,id',
+            'name' => 'required|unique|max:16',
+            'description' => 'required|max:255',
+        ]);
+
+
+        Item::create($request->all());
+
+        return redirect()->route('ResourceCRUD.index')
+                        ->with('success','Item created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Item::find($id);
+        return view('ResourceCRUD.show',compact('item'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = Item::find($id);
+        return view('ResourceCRUD.edit',compact('item'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'group_id' => 'required|exists:group,id',
+            'name' => 'required|unique|max:16',
+            'description' => 'required|max:255',
+        ]);
+
+
+        Item::find($id)->update($request->all());
+
+        return redirect()->route('ResourceCRUD.index')
+                        ->with('success','Item updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Item::find($id)->delete();
+        return redirect()->route('ResourceCRUD.index')
+                        ->with('success','Item deleted successfully');
+    }
+}
